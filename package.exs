019f489e1091986c8fa33e9992a1e4111ce017c8
woxtu <woxtup@gmail.com>
2014-04-11 12:39:22 +0900

    Define a package

---
+++ package.exs
@@ -0,0 +1,5 @@
+Expm.Package.new(name: "elixir-mbcs",
+                 description: "Wrapper for erlang-mbcs",
+                 version: "0.1.0",
+                 maintainers: [[name: "woxtu", email: "woxtup@gmail.com"]],
+                 repositories: [[github: "woxtu/elixir-mbcs"]])
