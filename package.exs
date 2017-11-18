7ced9eefcab52f209412ac399c67ab708393e1aa
woxtu <woxtup@gmail.com>
2017-11-18 20:20:30 +0900

    Delete unnecessary files

--- package.exs
+++
@@ -1,5 +0,0 @@
-Expm.Package.new(name: "elixir-mbcs",
-                 description: "Wrapper for erlang-mbcs",
-                 version: "0.1.1",
-                 maintainers: [[name: "woxtu", email: "woxtup@gmail.com"]],
-                 repositories: [[github: "woxtu/elixir-mbcs"]])
